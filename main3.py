84ca4042d2175741aeabe7f1499061fd3a9b6526
seayurre <35357896+seayurre@users.noreply.github.com>
2021-11-04 12:40:17 +0900

    Add files via upload
    index/f값/chk(이 점을 방문했으면 1이고 방문 안 했으면 0-즉 책에서 Q에 안 들어가면 1이고 들어가면 0)
    for문으로 chk=0인 것들 중에서 f가 가장 작은 걸 찾아서 return
    
    f값을 변화시킬 때는? index로 바로 접근 가능
    
    가로 길이 2
    세로 길이 2000 정도인 2차원 리스트를 만들자
    시간은 좀 오래 걸려도 실행이 제대로 될 수 있다!
    
    <해야 할 일>
    1. index와 f값과 chk를 묶어서 점들을 다시 정리한다
    f[i][0] : f(i)
    f[i][1] : 지나갔는지 안지나갔는지 
    2. 이걸 이용해서 A* 알고리즘을 다시 짜 본다(pq 사용하지 않고)
    
    -맨 처음에 모든 점들의 chk=0으로 초기화해두고
    g,h,f 초기값 초기화
    
    모든 점의 chk가 1이 되면 finish
    리스트 상에서 f가 가장 작은 걸 찾고 그 점의 index==종점명_index이면 finish
    그 점과 연결된 점 모두 보면서 g값 갱신, chk값도 갱신

--- main3.py
+++ main3.py
@@ -4,17 +4,19 @@ mem = psutil.Process() #저장공간 계산
 import time
 import HR1
 
-f = open("C:/Users/HOME/Desktop/PythonWorkspace/2021 2학기 과제연구/20210901 도로_가공.txt", 'r',encoding="cp949")
-lines = f.readlines()
+ff = open("C:/Users/yulyu/Desktop/과제연구 집탐 코드/20210901 도로_가공.txt", 'r',encoding="cp949")
+lines = ff.readlines()
 #도로 데이터에서 시점명과 종점명이 누락되어 있는 것을 발견함-서울시와 다른 지역 사이 경계에 있는 도로이거나 마땅한 이름이 없는 것으로 추정됨
 #이런 데이터들은 삭제하였음(수작업)
-f.close()
+ff.close()
 
 #dictionary value로 key값 얻기
 def value_to_key(val):
     for key, value in 지명_index.items():
          if val == value:
              return key
+
+
 
 ###############################################
 #휴리스틱 함수 결정하는 변수 p
@@ -33,15 +35,10 @@ for line in lines:
         지명_index[종점명] = index
         index+=1
 
-f =[]
+
 
 #그래프 연결 관계 나타내기
-graph=list() #인접행렬
-for x in range(2000):
-    f.append(0) #f 초기화
-    graph.append([])
-    for y in range(2000):
-        graph[x].append(0)
+graph = {}
 
 for line in lines:
     시점명, 종점명, 시간=line.split(',')
@@ -49,51 +46,67 @@ for line in lines:
     # 종점명.strip() #시점명과 종점명 띄어쓰기 삭제
     시점명_index = 지명_index[시점명]
     종점명_index = 지명_index[종점명]
-    graph[시점명_index][종점명_index] = float(시간) 
+    시간 = float(시간)
+    temp = [] #그래프에 추가해줄 이어질 노드와 노드까지의 가중치
+    temp.append(종점명_index)
+    temp.append(시간)
+    if 시점명_index not in graph.keys():
+        graph[시점명_index] = []
+        graph[시점명_index].append(temp)
+    else:
+        graph[시점명_index].append(temp)
+
 
 path = []
 timee = 0
-
-
-def Astar(start, finish): #start:시점명, finish:종점명
-    q = PriorityQueue()
-    q.put((1,start)) #start의 f값, start
-    global graph, path, f, g, h, timee
-    f[start] = 1 #start의 f값 ######여기서 f값만 바꿀 수 있는 방법을 찾으면 됨 ! - 파이썬에서 str은 바뀌지 않는다는데... 지금까지 계속 바꾸지 않았나
-
-    # if current == start:
-    #     for i in range(1813):
-    #         g.append(1e9)
-    #         f.append(1e9)
-    #         h.append(0)
-    #     g[start] = 0
-    #     path.append(start)
-
-    found = False
-    while not q.empty():
-        if found:
-            break
-        current = q.get()[1]
-        path.append(current)
-        for i in range(1813):
-            if graph[current][i] != 0:
-                h =  HR1.휴리스틱1(value_to_key(current), value_to_key(i),p) 
-                if f[i] > f[current] + graph[current][i] + h:
-                    f[i] = f[current] + graph[current][i] + h
-                    q.put((f[i], i))
-                if i == finish:
-                    f[i] = f[current] + graph[current][i]
-                    found = True
+global g,h,f
+g=[]
+h=[]
+f=[]
 
 s, f = input().split()
 start = 지명_index[s]
 finish = 지명_index[f]
-Astar(start, finish)
+
+
+pq = PriorityQueue()
+########start의 f값 ######여기서 f값만 바꿀 수 있는 방법을 찾으면 됨 ! - 파이썬에서 str은 바뀌지 않는다는데... 지금까지 계속 바꾸지 않았나
+    
+for i in range(1813):
+    g.append(1e9)
+    f.append(1e9)
+    h.append(0)
+    g[start] = 0
+    path.append(start)
+
+f[start] =h[start]
+pq.put((f[start],start)) #start의 f값, start
+
+found = False
+while not pq.empty():
+    if found:
+        print("도착!")
+        break
+    current = pq.get()[1] #어차피 index 알고 있으니까 q.get()[0]값은 안 가져와도 됨
+    path.append(current)
+
+
+    for v in graph[current]:
+        i=v[0]#인덱스
+        ga=v[1]#가중치
+        if g[i] > g[current] + graph[current][i]:
+            g[i]=g[current]+graph[current][i]
+            f[i] = g[i] + h[i]
+            pq.put((f[i], i))
+        if i == finish:
+            f[i] = f[current] + graph[current][i]
+            found = True
+
 
 for i in path:
     print(value_to_key(i))
 
-print("시간 : {}".format(f[finish]))
+print("시간 : {}".format(f[finish])) 
     
 
 
